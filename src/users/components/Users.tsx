57ef17a0d0d846f8589d0b1a40015025ff598afe
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-07-30 01:16:58 +0530

    zod implements

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,3 +1,25 @@
+import { useForm } from "react-hook-form";
+
 export const Users = () => {
-  return <div>Users</div>;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<{ email: string }>({ mode: "all" });
+
+  const onSubmit = () => {
+    console.log("submit");
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        placeholder="email"
+        {...register("email", {
+          required: { value: true, message: "Email is required" },
+          maxLength: { value: 10, message: "Email is too long" },
+        })}
+      />
+      <p>{errors.email?.message}</p>
+    </form>
+  );
 };
---
+++ src/users/types/schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+import { patterns } from "../../constants";
+
+export const schema = z.object({
+  name: z.string().min(1, { message: "required" }),
+  email: z
+    .string()
+    .min(3, { message: "Minimum 3 character is required" })
+    .refine((email) => patterns.email.test(email), {
+      message: "email not valid",
+    }),
+});
