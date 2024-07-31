869ed0bb93a07d2ad9bbd20a225d47e29b17231a
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-07-31 17:51:56 +0530

    Radio Button

---
+++ src/components/RHFRadiosGroup.tsx
@@ -0,0 +1,42 @@
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
+import { Controller, FieldValues, Path, useFormContext } from "react-hook-form";
+
+type Props<T extends FieldValues> = {
+  name: Path<T>;
+  options: Option[];
+  label: string;
+};
+export function RHFRadiosGroup<T extends FieldValues>({
+  name,
+  options = [],
+  label,
+}: Props<T>) {
+  const { control } = useFormContext();
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState: { error } }) => (
+        <FormControl {...field} error={!!error}>
+          <FormLabel>{label}</FormLabel>
+          <RadioGroup>
+            {options?.map((option) => (
+              <FormControlLabel
+                value={option.id}
+                key={option.id}
+                label={option.label}
+                control={<Radio checked={field.value === option.id} />}
+              />
+            ))}
+          </RadioGroup>
+        </FormControl>
+      )}
+    ></Controller>
+  );
+}
